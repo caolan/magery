04b46894fe9e8bd080e51b7a3b8fef93e48966d9
Caolan McMahon <caolan.mcmahon@gmail.com>
2017-06-26 16:40:44 +0100

    remove some unused code

--- magery.js
+++ magery.js
@@ -1,20 +1,11 @@
 "use strict";
 
-// var parsers = require('./parsers');
 // var context = require('./context');
 var render = require('./render');
 var patch = require('./patch');
 
 
 /***** Public API *****/
-
-// exports.loadTemplates = parsers.loadTemplates;
-
-// exports.patch = function (templates, name, node, next_data, prev_data, first_pass) {
-//     var patcher = new patch.Patcher(node);
-//     var renderer = new render.Renderer(patcher, templates, first_pass);
-//     renderer.render(name, next_data, prev_data);
-// };
 
 function BoundTemplate(node, template, data, handlers) {
     this.node = node;
