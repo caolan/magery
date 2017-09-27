2dd870a1cfb089d3bf850575b2c079bcf6fb468a
Caolan McMahon <caolan.mcmahon@gmail.com>
2017-09-27 12:20:49 +0100

    Remove old render.js file from webpack config

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     entry: {
         'compile': ['./src/compile.js'],
-        'render': ['./src/render.js'],
         'patch': ['./src/patch.js'],
         'transforms': ['./src/transforms.js'],
         'utils': ['./src/utils.js'],
